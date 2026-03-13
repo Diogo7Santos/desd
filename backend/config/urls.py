641e358aa06bb08db0d8a3bc1e1c792f7bb03c81
ChingHeiYuCasper <ching4.yu@live.uwe.ac.uk>
2026-03-13 16:02:45 +0000

    Remove duplicate accounts.urls from urlpatterns
    Removed duplicate inclusion of accounts.urls in urlpatterns.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -8,11 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", lambda request: redirect("catalog:product_list")),
     path("admin/", admin.site.urls),
-    path("", include("accounts.urls")),
     path("catalog/", include("catalog.urls")),
     path("p/", include("payments.urls")),
     path("", include("accounts.urls")), # Web views for accounts (login, register, dashboards)
-    path("api/accounts/", include("accounts.urls")), #DRF endpoints for accounts
 ]
 
 if settings.DEBUG:
