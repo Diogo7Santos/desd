41f9dce88c4047234f5f8bc19e5fba5ab100fa31
SimonHJ22 <si2.Tun@live.uwe.ac.uk>
2026-03-17 17:22:06 +0000

    payment update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 from celery.schedules import crontab
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-REPO_DIR = BASE_DIR.parent # Assuming the structure is repo/backend/config/settings.py
+REPO_DIR = BASE_DIR.parent  # repo/
 
 # Load .env from the repo root (desd/.env)
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(REPO_DIR / ".env")
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key-change-me")
 
